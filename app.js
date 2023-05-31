5c7bb2c4b0f5bcedf95a45f3a1807281ec418a8c
Philip Reich <philip.a.reich@gmail.com>
2023-05-31 11:28:16 +0200

    v0.1

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import * as THREE from "three";
-import * as d3 from "d3";
 import * as Papa from "papaparse";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const resolution = 512
+const resolution = 32
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -22,30 +21,6 @@ const material1 = new THREE.MeshBasicMaterial( { map: texture } );
 const earth = new THREE.Mesh( geometry1, material1 );
 scene.add( earth );
 
-
-let apogee = 3832 * scaleFactor + earthradius;
-let perigee = 649 * scaleFactor + earthradius;
-
-let semiMajorAxis = (apogee + perigee) / 2;  
-let semiMinorAxis = Math.sqrt(apogee * perigee);  
-let inclination = 34.24;  // In degrees
-
-let orbitShape = new THREE.EllipseCurve(
-    0,  0,            // ax, aY
-    semiMajorAxis, semiMinorAxis,           // xRadius, yRadius
-    0,  2 * Math.PI,  // aStartAngle, aEndAngle
-    false,            // aClockwise
-    0                 // aRotation
-);
-
-let points = orbitShape.getPoints(resolution);
-let geometry = new THREE.BufferGeometry().setFromPoints(points);
-let material = new THREE.LineBasicMaterial({color: 0xff0000});
-let ellipse = new THREE.Line(geometry, material);
-ellipse.rotation.x = THREE.MathUtils.degToRad(90);  // rotation to make it 3D
-ellipse.rotation.y = THREE.MathUtils.degToRad(inclination); // apply inclination
-scene.add(ellipse);
-
 const controls = new OrbitControls( camera, renderer.domElement );
 camera.position.set( 1, 1, 2 );
 controls.update();
@@ -56,6 +31,51 @@ controls.maxDistance = 10;
 controls.minDistance = 1.2;
 
 
+function orbitShapeMaker(semiMajorAxis, semiMinorAxis) {
+    return new THREE.EllipseCurve(
+        0,  0,            // center
+        semiMajorAxis, semiMinorAxis,           // xRadius, yRadius
+        0,  2 * Math.PI,  // aStartAngle, aEndAngle
+        false,            // aClockwise
+        0, // aRotation
+    );
+}
+
+function drawOrbit(apogee, perigee, inclination) {
+    let semiMajorAxis = (apogee + perigee) / 2;  
+    let semiMinorAxis = Math.sqrt(apogee * perigee);  
+    let orbit = orbitShapeMaker(semiMajorAxis, semiMinorAxis);
+    let points = orbit.getPoints(resolution);
+    let geometry = new THREE.BufferGeometry().setFromPoints(points);
+    let material = new THREE.LineBasicMaterial({color: 0xff0000});
+    let ellipse = new THREE.Line(geometry, material);
+    ellipse.rotation.x = (90 + inclination) * Math.PI / 180;
+    //ellipse.rotation.y = yRotation * Math.PI / 180;
+    scene.add(ellipse);
+}
+
+// make x y z axis visible
+
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+
+fetch('data/satcat.csv')
+    .then(response => response.text())
+    .then(data => {
+        let results = Papa.parse(data, { header: true });
+
+        let filteredData = results.data.filter(row => row['ORBIT_TYPE'] === 'ORB'&& row['OBJECT_TYPE'] === 'PAY').slice(0, 3);
+
+        filteredData.forEach(row => {
+            let apogee = parseFloat(row['APOGEE']) * scaleFactor + earthradius;
+            let perigee = parseFloat(row['PERIGEE']) * scaleFactor + earthradius;
+            let inclination = parseFloat(row['INCLINATION']);
+            
+            drawOrbit(apogee, perigee, inclination);
+        });
+});
+
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
@@ -64,7 +84,7 @@ function onWindowResize() {
 
 function animate() {
     // rotate the earth
-    earth.rotation.y += 0.0002;
+    earth.rotation.y += 0.0000;
 	requestAnimationFrame( animate );
     let distance = camera.position.length();
     controls.rotateSpeed = distance * 0.3;
--- updater.py
+++ updater.py
@@ -6,7 +6,7 @@ import os
 def update():
     url = 'https://celestrak.org/pub/satcat.csv'
     r = requests.get(url, allow_redirects=True)
-    open('data/satcat.csv', 'wb').write(r.content)
+    open('public/satcat.csv', 'wb').write(r.content)
     print('satcat.csv updated')
 
 if __name__ == '__main__':
