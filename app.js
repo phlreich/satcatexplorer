e0eb32d914319735957fa3bb101899525e605120
Philip Reich <philip.a.reich@gmail.com>
2023-05-31 19:38:58 +0200

    fixed the orbit calculation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const resolution = 32
+const resolution = 128;
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,60 +21,69 @@ const material1 = new THREE.MeshBasicMaterial( { map: texture } );
 const earth = new THREE.Mesh( geometry1, material1 );
 scene.add( earth );
 
-const controls = new OrbitControls( camera, renderer.domElement );
-camera.position.set( 1, 1, 2 );
-controls.update();
-controls.enablePan = false;
-controls.enableDamping = true;
-controls.zoomSpeed = 1;
-controls.maxDistance = 10;
-controls.minDistance = 1.2;
-
-
 function orbitShapeMaker(semiMajorAxis, semiMinorAxis) {
+    // scale everything
+    semiMajorAxis *= scaleFactor;
+    semiMinorAxis *= scaleFactor;
     return new THREE.EllipseCurve(
-        0,  0,            // center
-        semiMajorAxis, semiMinorAxis,           // xRadius, yRadius
-        0,  2 * Math.PI,  // aStartAngle, aEndAngle
-        false,            // aClockwise
-        0, // aRotation
+        Math.sqrt(semiMajorAxis**2 - semiMinorAxis**2), 0,                         // center
+        semiMajorAxis, semiMinorAxis,   // xRadius, yRadius
+        0,  2 * Math.PI,                // aStartAngle, aEndAngle
+        false,                          // aClockwise
+        0,                              // aRotation
     );
 }
-
-function drawOrbit(apogee, perigee, inclination) {
-    let semiMajorAxis = (apogee + perigee) / 2;  
-    let semiMinorAxis = Math.sqrt(apogee * perigee);  
-    let orbit = orbitShapeMaker(semiMajorAxis, semiMinorAxis);
-    let points = orbit.getPoints(resolution);
-    let geometry = new THREE.BufferGeometry().setFromPoints(points);
-    let material = new THREE.LineBasicMaterial({color: 0xff0000});
-    let ellipse = new THREE.Line(geometry, material);
-    ellipse.rotation.x = (90 + inclination) * Math.PI / 180;
-    //ellipse.rotation.y = yRotation * Math.PI / 180;
-    scene.add(ellipse);
-}
-
-// make x y z axis visible
-
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
-
 
 fetch('data/satcat.csv')
     .then(response => response.text())
     .then(data => {
         let results = Papa.parse(data, { header: true });
 
-        let filteredData = results.data.filter(row => row['ORBIT_TYPE'] === 'ORB'&& row['OBJECT_TYPE'] === 'PAY').slice(0, 3);
+        let filteredData = results.data.filter(row => 
+            row['ORBIT_TYPE'] === 'ORB'&&
+            row['OBJECT_TYPE'] === 'PAY' &&
+            row['ORBIT_CENTER'] === 'EA').slice(0,1);
 
         filteredData.forEach(row => {
-            let apogee = parseFloat(row['APOGEE']) * scaleFactor + earthradius;
-            let perigee = parseFloat(row['PERIGEE']) * scaleFactor + earthradius;
-            let inclination = parseFloat(row['INCLINATION']);
-            
-            drawOrbit(apogee, perigee, inclination);
+            fetch(`https://celestrak.com/NORAD/elements/gp.php?CATNR=${row['NORAD_CAT_ID']}&FORMAT=CSV`)
+                .then(response => response.text())
+                .then(data => {
+                    let results = Papa.parse(data, { header: true });
+                    console.log(results.data[0]);
+                    let mu = 398600.4418; // Earth's gravitational parameter, in km^3/s^2
+                    let mean_motion = parseFloat(results.data[0]['MEAN_MOTION']);
+                    let n = mean_motion * 2 * Math.PI / (24*3600); // convert from rev/day to rad/s
+                    let semiMajorAxis = Math.pow(mu / (n * n), 1 / 3);
+                    let eccentricity = parseFloat(results.data[0]['ECCENTRICITY']);
+                    let semiMinorAxis = semiMajorAxis * Math.sqrt(1 - eccentricity * eccentricity);
+                    let ARG_OF_PERICENTER = parseFloat(results.data[0]['ARG_OF_PERICENTER']);
+                    let orbit = orbitShapeMaker(semiMajorAxis, semiMinorAxis);
+                    let points = orbit.getPoints(resolution);
+                    let geometry = new THREE.BufferGeometry().setFromPoints(points);
+                    let material = new THREE.LineBasicMaterial({color: 0xff0000});
+                    let ellipse = new THREE.Line(geometry, material);
+                    let INCLINATION = parseFloat(results.data[0]['INCLINATION']);
+                    let RA_OF_ASC_NODE = parseFloat(results.data[0]['RA_OF_ASC_NODE']);
+                    ellipse.rotation.x = THREE.MathUtils.degToRad(90 - INCLINATION); // you guessed it, the axes are all messed up
+                    ellipse.rotation.z = THREE.MathUtils.degToRad(RA_OF_ASC_NODE);
+                    scene.add(ellipse);
+                });
         });
 });
+
+// make x y z axis visible
+
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 0, 3, 0 );
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+controls.zoomSpeed = 1;
+controls.maxDistance = 100;
+controls.minDistance = 1.2;
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -91,6 +100,8 @@ function animate() {
     controls.update();
 	renderer.render( scene, camera );
     onWindowResize();
+    // console.log(camera.position);
 }
 
+console.log(scene.children);
 animate();
--- updater.py
+++ updater.py
@@ -1,13 +1,19 @@
 # get data from https://celestrak.org/pub/satcat.csv and write to data folder as satcat.csv
 
 import requests
-import os
 
 def update():
     url = 'https://celestrak.org/pub/satcat.csv'
     r = requests.get(url, allow_redirects=True)
-    open('public/satcat.csv', 'wb').write(r.content)
+    open('public/data/satcat.csv', 'wb').write(r.content)
     print('satcat.csv updated')
+
+    # the same for https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv
+
+    url = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv'
+    r = requests.get(url, allow_redirects=True)
+    open('public/data/active.csv', 'wb').write(r.content)
+    print('active.csv updated')
 
 if __name__ == '__main__':
     update()
