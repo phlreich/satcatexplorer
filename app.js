3384863ec919daad33f5aa920821c08366d15beb
Philip Reich <philip.a.reich@gmail.com>
2023-06-01 23:46:27 +0200

    minor refactor

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const resolution = 256;
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -15,22 +14,30 @@ const earthradius = 1;
 const scaleFactor = earthradius / 6371;
 const geometry1 = new THREE.SphereGeometry(earthradius, resolution, resolution);
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load("/2k_custom_earth_daymap.webp");
+const texture = textureLoader.load("/8081_earthmap10k.webp");
 texture.colorSpace = THREE.SRGBColorSpace; // use srgb color space
 const material1 = new THREE.MeshBasicMaterial( { map: texture } );
 const earth = new THREE.Mesh( geometry1, material1 );
 scene.add( earth );
 
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( .3, 4, 4 );
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+controls.zoomSpeed = 1;
+controls.maxDistance = 100;
+controls.minDistance = 1.2;
+
 function orbitShapeMaker(semiMajorAxis, semiMinorAxis) {
-    // scale everything
     semiMajorAxis *= scaleFactor;
     semiMinorAxis *= scaleFactor;
     return new THREE.EllipseCurve(
-        Math.sqrt(semiMajorAxis**2 - semiMinorAxis**2), 0,                         // center
-        semiMajorAxis, semiMinorAxis,   // xRadius, yRadius
-        0,  2 * Math.PI,                // aStartAngle, aEndAngle
-        false,                          // aClockwise
-        0,                              // aRotation
+        Math.sqrt(semiMajorAxis**2 - semiMinorAxis**2), 0,        // center
+        semiMajorAxis, semiMinorAxis,                             // xRadius, yRadius
+        0,  2 * Math.PI,                                          // aStartAngle, aEndAngle
+        false,                                                    // aClockwise
+        0,                                                        // aRotation
     );
 }
 
@@ -44,53 +51,81 @@ function orbitMaker(mean_motion, eccentricity, INCLINATION, RA_OF_ASC_NODE) {
     let geometry = new THREE.BufferGeometry().setFromPoints(points);
     let material = new THREE.LineBasicMaterial({color: 0xff0000});
     let ellipse = new THREE.Line(geometry, material);
-    ellipse.rotation.x = THREE.MathUtils.degToRad(90);
-    ellipse.rotation.z = THREE.MathUtils.degToRad(RA_OF_ASC_NODE);
-    ellipse.rotation.y = THREE.MathUtils.degToRad(INCLINATION);
+    let x = THREE.MathUtils.degToRad(90);
+    let y = THREE.MathUtils.degToRad(RA_OF_ASC_NODE);
+    let z = THREE.MathUtils.degToRad(INCLINATION);
+    ellipse.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), x); // rotation order is important
+    ellipse.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), z);
+    ellipse.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), y);
     return ellipse;
 }
 
-let k = 27;
+function drawOrbitFromData(results) {
+    let mean_motion = parseFloat(results['MEAN_MOTION']);
+    let eccentricity = parseFloat(results['ECCENTRICITY']);
+    let INCLINATION = parseFloat(results['INCLINATION']);
+    let RA_OF_ASC_NODE = parseFloat(results['RA_OF_ASC_NODE']);
+    let orbit = orbitMaker(mean_motion, eccentricity, INCLINATION, RA_OF_ASC_NODE);
+    scene.add(orbit);
+}
 
-fetch('data/satcat.csv')
-    .then(response => response.text())
-    .then(data => {
-        let results = Papa.parse(data, { header: true });
 
-        let filteredData = results.data.filter(row => 
-            row['ORBIT_TYPE'] === 'ORB'&&
-            row['OBJECT_TYPE'] === 'PAY' &&
-            row['ORBIT_CENTER'] === 'EA').slice(k, k+1);
+let activeData = [];
+let satcatData = [];
 
-        filteredData.forEach(row => {
-            fetch(`https://celestrak.com/NORAD/elements/gp.php?CATNR=${row['NORAD_CAT_ID']}&FORMAT=CSV`)
-                .then(response => response.text())
-                .then(data => {
-                    let results = Papa.parse(data, { header: true });
-                    console.log(results.data[0]);
-                    let mean_motion = parseFloat(results.data[0]['MEAN_MOTION']);
-                    let eccentricity = parseFloat(results.data[0]['ECCENTRICITY']);
-                    let INCLINATION = parseFloat(results.data[0]['INCLINATION']);
-                    let RA_OF_ASC_NODE = parseFloat(results.data[0]['RA_OF_ASC_NODE']);
-                    let orbit = orbitMaker(mean_motion, eccentricity, INCLINATION, RA_OF_ASC_NODE);
-                    scene.add(orbit);
-                });
+function parseCSVData(url) {
+    return new Promise((resolve, reject) => {
+        Papa.parse(url, {
+            download: true,
+            header: true,
+            complete: function(results) {
+                resolve(results.data);
+            },
+            error: function(err) {
+                reject(err);
+            }
         });
-});
+    });
+}
 
-// make x y z axis visible
+async function updateData() {
+    try {
+        activeData = await parseCSVData("https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv");
+        satcatData = await parseCSVData("https://celestrak.com/pub/satcat.csv");
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+async function readDataFromDisk() {
+    try {
+        activeData = await parseCSVData("data/active.csv");
+        satcatData = await parseCSVData("data/satcat.csv");
+    } catch (err) {
+        console.error(err);
+    }
+    satcatData = satcatData.filter(item => {
+        return activeData.find(activeItem => activeItem.NORAD_CAT_ID === item.NORAD_CAT_ID);
+    }
+    );
+}
 
-const controls = new OrbitControls( camera, renderer.domElement );
-camera.position.set( 2, 3, 2 );
-controls.update();
-controls.enablePan = false;
-controls.enableDamping = true;
-controls.zoomSpeed = 1;
-controls.maxDistance = 100;
-controls.minDistance = 1.2;
+function filterDataAndDraw() {
+    let filteredData = satcatData.filter(item => {
+        return item.OBJECT_TYPE === 'PAY' &&
+            item.OBJECT_NAME.includes('COM') &&
+            new Date(item.LAUNCH_DATE).getTime() < new Date(1999, 0, 1).getTime()
+    });
+    filteredData.forEach(item => {
+        let activeItem = activeData.find(activeItem => activeItem.NORAD_CAT_ID === item.NORAD_CAT_ID);
+        if (activeItem) {
+            drawOrbitFromData(activeItem);
+        }
+    }
+    );
+}
+
+readDataFromDisk().then(filterDataAndDraw).catch(err => console.error(err));
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -99,16 +134,13 @@ function onWindowResize() {
 }
 
 function animate() {
-    // rotate the earth
-    earth.rotation.y += 0.0000;
+    earth.rotation.y += 0.0005;
 	requestAnimationFrame( animate );
     let distance = camera.position.length();
     controls.rotateSpeed = distance * 0.3;
     controls.update();
 	renderer.render( scene, camera );
     onWindowResize();
-    // console.log(camera.position);
 }
 
-console.log(scene.children);
 animate();
