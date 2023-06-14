c1e670328021fac4c30e2e695a2c9cf8514755ec
Philip Reich <philip.a.reich@gmail.com>
2023-06-14 21:57:03 +0200

    whoops

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const pool = new Pool({
 const userpool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: DB_DATABASE,
+    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
